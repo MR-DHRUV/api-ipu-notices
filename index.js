7679155f7b5445e7375fd2d67725173550087aee
MR-DHRUV <dhruvgupta742@gmail.com>
2022-09-27 19:54:52 +0530

    Stable 1.0

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 require('dotenv').config()
 const axios = require('axios')
 const cheerio = require('cheerio')
-const cors = require('cors')
 const express = require("express");
 const app = express();
+const cors = require("cors")
 const port = 5000;
 const bodyparser = require('body-parser');
 const { request } = require('express');
 const { body, validationResult } = require('express-validator');
+var nodemailer = require('nodemailer');
+const mailPass = process.env.EMAIL_PASS2;
 
 app.use(express.static('static'))
 app.use(bodyparser.urlencoded())
 app.use(express.static('static'))
 app.use(bodyparser.json()); // support json encoded bodies
 app.use(bodyparser.urlencoded({ extended: true })); // support encoded bodies
+app.use(cors())
 
 const authifyEncrytionController = process.env.ENCYPTIONID;
 
@@ -118,10 +121,10 @@ app.get('/', async (req, res) => {
 
 
 
-app.get('/url', [
+app.post('/url', [
     body('url', "Please enter a valid url").isLength({ min: 10 }),
     body('url', "Please enter a valid url").isURL(),
-    body('authifyEncrytion', "Please enter a valid encyption id").isLength({min : 34}),
+    body('authifyEncrytion', "Please enter a valid encyption id").isLength({ min: 34 }),
 
 ], async (req, res) => {
 
@@ -131,9 +134,8 @@ app.get('/url', [
         return res.status(400).json({ error: errorsInInput.array() });
     }
 
-    if(req.body.authifyEncrytion != authifyEncrytionController)
-    {
-        return res.status(404).json({msg : "NOT ALLOWED"})
+    if (req.body.authifyEncrytion != authifyEncrytionController) {
+        return res.status(404).json({ msg: "NOT ALLOWED" })
     }
 
     try {
@@ -229,6 +231,68 @@ app.get('/url', [
         res.json(error);
     }
 })
+
+const authifyMailer = (to, sub = "Bienvenue à bord! You have been subscribed for new notices and updates", body = "Bienvenue à bord! We are glad to inform you that you have been subscribed now for new notices and updates. As soon as a new notice comes up, you'll receive a notification from Us.\n\nThank you\nError_404") => {
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'developer.authify@gmail.com',
+            pass: mailPass
+        }
+    });
+
+    var mailOptions = {
+        from: 'developer.authify@gmail.com',
+        to: to,
+        subject: sub,
+        text: body,
+    };
+
+    try {
+        transporter.sendMail(mailOptions, function (error, info) {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log('Email sent: ' + info.response);
+                res.status(200).json({ success: true });
+                return true
+            }
+        });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Some error occured");
+    }
+
+}
+
+app.post('/notifyme', [
+    body('to', "Please enter a valid email").isLength({ min: 10 }),
+    body('to', "Please enter a valid email").isEmail(),
+], async (req, res) => {
+
+    const errorsInInput = validationResult(req);
+    if (!errorsInInput.isEmpty()) {
+        return res.status(400).json({ error: errorsInInput.array() });
+    }
+
+    try {
+
+        if (req.body.new == "1") {
+            await authifyMailer(req.body.to, "Alert! New Notice has been posted on the website.", "You have received a new notice on the website. To look more into the notice, What's it's all about? Kindly visit the website\n\nThank you\nError_404");
+            res.status(200).json({ success: true });
+        }
+        else {
+            await authifyMailer(req.body.to);
+            res.status(200).json({ success: true });
+        }
+
+    }
+    catch (error) {
+        res.json(error)
+    }
+
+})
+
 
 
 app.listen(process.env.PORT || port, () => {
